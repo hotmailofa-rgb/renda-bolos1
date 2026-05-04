7e6b944232b29560605dda1ff1ca2752d95ab7a4
hotmailofa-rgb <hotmailofa@gmail.com>
2026-05-04 00:11:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Site no ar 🚀</h1>
-      <p>Se você está vendo isso, o deploy funcionou.</p>
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>🍰 Ganhe dinheiro com bolos simples</h1>
+      <p>Aprenda como fazer até R$100 por dia mesmo começando do zero</p>
+
+      <a
+        href="https://pay.hotmart.com/E78386330G"
+        style={{
+          display: "inline-block",
+          marginTop: "20px",
+          padding: "15px 25px",
+          background: "green",
+          color: "#fff",
+          borderRadius: "8px",
+          textDecoration: "none",
+        }}
+      >
+        Quero começar agora
+      </a>
     </div>
   );
 }
